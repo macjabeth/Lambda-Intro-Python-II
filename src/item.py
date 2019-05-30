dfad22c108e297e0fa9c0ee3f627bd321a67aef0
Jonathan Picazo <macbethjonathan@gmail.com>
2019-05-30 19:01:15 -0400

    add item module

---
+++ src/item.py
@@ -0,0 +1,21 @@
+import itertools
+
+class Item():
+    new_id = itertools.count().__next__
+
+    def __init__(self, **args):
+        self.id = Item.new_id()
+        self.replica = args['replica']
+        self.name = args['name']
+        self.description = args['description']
+        self.room_desc = args['room_desc']
+        self.keywords = (self.replica, *args['aliases'])
+
+    @staticmethod
+    def contains(items, thing):
+        # last word is most likely the item we need
+        # this way we handle cases like `get silver knife`
+        thing = thing.split()[-1]
+        for item in items:
+            if thing in item.keywords:
+                return item
